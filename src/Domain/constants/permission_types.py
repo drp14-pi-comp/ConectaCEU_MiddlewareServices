b34da968fdf5f74bc81cccfa1e3d4ead8c799d21
Patrick <patrick@datamotion.com.br>
2026-04-28 01:14:21 -0300

    added permission_types constants

---
+++ src/Domain/constants/permission_types.py
@@ -0,0 +1,9 @@
+class PermissionTypes:
+    REGISTER_USER = "register_user",
+    VALIDATE_USER_DOCUMENTS = "validate_user_documents",
+    LIST_SECRETARIES = "list_secretaries",
+    LIST_EDUCATORS = "list_educators",
+    LIST_STUDENTS = "list_students",
+    SEND_BROADCAST_MESSAGE = "send_broadcast_message",
+    ADD_COURSES = "add_courses",
+    ADD_CLASSES = "add_classes"
