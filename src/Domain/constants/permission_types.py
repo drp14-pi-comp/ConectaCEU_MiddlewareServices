ed40f24355c8989f6a13b05721e434d15aa0986b
Patrick <patrick@datamotion.com.br>
2026-05-10 16:26:08 -0300

    added typing to PermissionTypes

--- src/Domain/constants/permission_types.py
+++ src/Domain/constants/permission_types.py
@@ -1,9 +1,9 @@
 class PermissionTypes:
-    REGISTER_USER = "register_user",
-    VALIDATE_USER_DOCUMENTS = "validate_user_documents",
-    LIST_SECRETARIES = "list_secretaries",
-    LIST_EDUCATORS = "list_educators",
-    LIST_STUDENTS = "list_students",
-    SEND_BROADCAST_MESSAGE = "send_broadcast_message",
-    ADD_COURSES = "add_courses",
-    ADD_CLASSES = "add_classes"
+    REGISTER_USER: str = "register_user",
+    VALIDATE_USER_DOCUMENTS: str = "validate_user_documents",
+    LIST_SECRETARIES: str = "list_secretaries",
+    LIST_EDUCATORS: str = "list_educators",
+    LIST_STUDENTS: str = "list_students",
+    SEND_BROADCAST_MESSAGE: str = "send_broadcast_message",
+    ADD_COURSES: str = "add_courses",
+    ADD_CLASSES: str = "add_classes"
